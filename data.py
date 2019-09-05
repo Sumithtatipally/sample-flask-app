8c70d96df84ea969e1bff1d763e79e9377d6b127
sumithtatipally <sumithtatipally@gmail.com>
2019-09-06 00:03:51 +0530

    static flask site sample

---
+++ data.py
@@ -0,0 +1,18 @@
+def Blogs():
+    blogs = [
+        {
+            'id':1,
+            'title':'Article One',
+            'body': 'This Article is base on Python Flask',
+            'author':'Sumith',
+            'create_data':'25-07-2018'
+        },
+        {
+            'id': 2,
+            'title': 'Article two',
+            'body': 'This Article is base on Python Flask',
+            'author': 'Sonu',
+            'create_data': '24-07-2018'
+        },
+    ]
+    return blogs
